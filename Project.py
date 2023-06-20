f100295b70aa3ea823bb5dd8856146acffbdff7e
gasterexon <nikitasapovalenko35@gmail.com>
2023-06-20 16:34:40 +0400

    Signed-off-by: gasterexon <nikitasapovalenko35@gmail.com>

--- Project.py
+++ Project.py
@@ -1,20 +1,8 @@
-# def strcounter(s):
-#     for symbol in set(s):
-#         counter = 0
-#         for sub_symbol in s:
-#             if symbol == sub_symbol:
-#                 counter += 1
+def checkPalindrome(n):
+    centr = len(n)//2
+    for i in range(centr):
+        if n[i]!=n[len(n)-1-i]:
+            return False
+    return True
 
-#         print(symbol, counter)
-
-# strcounter('aaadbcd')
-
-def strcounter(s):
-    syms_counter = {}
-    for symbol in s:
-        syms_counter[symbol] = syms_counter.get(symbol, 0) + 1
-
-    for symbol, count in syms_counter.items():
-        print(symbol, count)
-
-strcounter('wodopad')
+print(checkPalindrome('лепсспел'))
